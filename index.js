7b8cf686cf2dc082a0135a05cec2a27f3d8cd6a8
Anders Thøgersen <anders@bladre.dk>
2015-11-06 14:07:07 +0100

    Enable defining tasks in groups

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-/*global console*/
 
-var fs = require('fs');
+"use strict"
+
+var fs = require('fs')
 
 function clone(dest) {
+    if ('string' === typeof dest) {
+        return dest;
+    }
     var r = {};
     if (Array.isArray(dest))
         r = [];
@@ -12,72 +16,89 @@ function clone(dest) {
     return r;
 }
 
-module.exports = function (gulp, conf) {
-    var projectDir = module.parent.filename.replace(/^(.+)\/[^\/\\]+$/, '$1');
-
-    var taskDirs = [ ];
-
-    // ./gulp/<script>.js
-    taskDirs.push(projectDir + '/' + (conf.taskDir || 'gulp') + '/');
-
-    // ./node_modules/<conf.(name|module|'gulp-sugar-simple')>/lib/<script>.js
-    taskDirs.push(projectDir + '/node_modules/' + (conf.name || conf.module ||
-                                              process.env.GULP_SUGAR_TASKS || 
-                                              'gulp-sugar-simple') + '/lib/');
-
-    // ./lib/<script>.js
-    taskDirs.push(projectDir + '/lib/');
-
-    // <script>.js
-    taskDirs.push('');
-
-    var added = 0;
-    for (var taskName in conf) {
-        if (taskName === 'dest' || taskName === 'src' || taskName === 'module')
-            continue;
-        var taskConf = clone(conf[taskName]);
-
-        // Allow placing .src and .dest on the parent object
-        if (!taskConf.src && conf.src) taskConf.src = conf.src;
-        if (!taskConf.dest && conf.dest) taskConf.dest = conf.dest;
-
-        var deps = taskConf.deps || [];
-        if ('string' === typeof deps) deps = [deps];
-
-        var taskScript = taskConf.task || taskName,
-            task = null,
-            taskPath = '';
-
-        if (taskConf.skip) {
-            task = function () {
-                var skip = taskName;
-                return function () {
-                    console.log('Skipping task', skip);
-                };
-            };
-        } else {
-            for (var i = 0; i < taskDirs.length; i += 1) {
-                taskPath = taskDirs[i] + taskScript + '.js';
-                if (fs.existsSync(taskPath)) {
-                    try {
-                        task = require(taskPath);
-                    } catch (e) {
-                        console.warn('Error loading "' + taskPath + '"\n\t' +
-                                                        e.message);
-                        return 1;
-                    }
-                }
-                if (task) break;
-            }
-        }
-
-        if ('function' !== typeof task) {
-            throw new Error("Couldn't find task " + taskScript +
-                            ' in \n\t' + taskDirs.join('\n\t'));
-        }
-        gulp.task(taskName, deps, task(gulp, taskConf));
-        added += 1; // TODO: Handle added tasks
+class Sugar {
+    constructor(gulp, o) {
+        this.gulp = gulp;
+        this.o = o;
     }
+    task(superTask, maybeDeps, o) {
+        if (!o) {
+            o = maybeDeps;
+            maybeDeps = [];
+        }
+        o = o || {};
+        var projectDir = module.parent.filename.replace(/^(.+)\/[^\/\\]+$/, '$1');
 
-    return gulp;
+        var taskDirs = [ ];
+
+        // ./gulp/<script>.js
+        taskDirs.push(projectDir + '/' + (o.taskDir || 'gulp') + '/');
+
+        // ./node_modules/<o.(name|module|'gulp-sugar-simple')>/lib/<script>.js
+        taskDirs.push(projectDir + '/node_modules/' + (o.name || o.module ||
+                                                  process.env.GULP_SUGAR_TASKS ||
+                                                  'gulp-sugar-simple') + '/lib/');
+
+        // ./lib/<script>.js
+        taskDirs.push(projectDir + '/lib/');
+
+        // <script>.js
+        taskDirs.push('');
+
+        var added = 0;
+        var taskNames = [];
+        for (var taskName in o) {
+            if (taskName === 'dest' || taskName === 'src' || taskName === 'module')
+                continue;
+            taskNames.push(taskName);
+            var taskConf = clone(o[taskName]);
+
+            // Allow placing .src and .dest on the parent object
+            if (!taskConf.src && o.src) taskConf.src = o.src;
+            if (!taskConf.dest && o.dest) taskConf.dest = o.dest;
+
+            var deps = taskConf.deps || [];
+            if ('string' === typeof deps) deps = deps.split(/\s+/);
+
+            var taskScript = taskConf.task || taskName,
+                task = null,
+                taskPath = '';
+
+            if (taskConf.skip) {
+                task = function () {
+                    var skip = taskName;
+                    return function () {
+                        console.log('Skipping task', skip);
+                    };
+                };
+            } else {
+                for (var i = 0; i < taskDirs.length; i += 1) {
+                    taskPath = taskDirs[i] + taskScript + '.js';
+                    if (fs.existsSync(taskPath)) {
+                        try {
+                            task = require(taskPath);
+                        } catch (e) {
+                            console.warn('Error loading "' + taskPath + '"\n\t' +
+                                                            e.message);
+                            return 1;
+                        }
+                    }
+                    if (task) break;
+                }
+            }
+
+            if ('function' !== typeof task) {
+                throw new Error("Couldn't find task " + taskScript +
+                                ' in \n\t' + taskDirs.join('\n\t'));
+            }
+            this.gulp.task(taskName, deps, task(this.gulp, taskConf));
+            added += 1; // TODO: Handle added tasks
+        }
+        return module.exports = this.gulp.task(superTask, maybeDeps.concat(taskNames));
+    }
+}
+
+module.exports = function (gulp) {
+    var s1 = new Sugar(gulp);
+    return s1;
 };
