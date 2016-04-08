dbf12e1443920a6ade9ad51ba3b5fd5fcc0de9b1
Anders Thøgersen <anders@bladre.dk>
2016-04-08 16:42:10 +0200

    better errors, more clear signature

--- index.js
+++ index.js
@@ -9,98 +9,122 @@ class Sugar {
   constructor (gulp, o) {
     this.gulp = gulp
     this.o = o
+    this.tasksSeen = {}
   }
-  task (superTask, maybeDeps, o) {
+  task (superTask, tasksOrDeps, o) {
+    var type = typeof tasksOrDeps
+    if (type === 'string' || Array.isArray(tasksOrDeps)) {
+      if (type === 'string') tasksOrDeps = tasksOrDeps.split(/\s+/)
+      this.gulp.task(superTask, tasksOrDeps)
+      this.gulp.task('default', [superTask])
+    }
+
     if (!o) {
-      o = maybeDeps
-      maybeDeps = []
-    }
-    o = o || {}
-    var projectDir = module.parent.filename.replace(/^(.+)\/[^\/\\]+$/, '$1')
-
-    var rootDir = findNodeModulesDir(projectDir)
-
-    var taskDirs = [ ]
-
-    // Check a handful of dirs for task definitions
-    taskDirs.push(rootDir + '/' + (o.taskDir || 'gulp') + '/')
-    if (process.env.GULP_SUGAR_TASKS) {
-      var envTasks = process.env.GULP_SUGAR_TASKS
-      if (envTasks[0] !== '/') {
-        envTasks = rootDir + '/' + envTasks
-      }
-      if (envTasks[envTasks.length - 1] !== '/') {
-        envTasks = envTasks + '/'
-      }
-      taskDirs.push(envTasks)
+      o = tasksOrDeps
+      tasksOrDeps = []
     }
 
-    if (o.name || o.module) {
-      taskDirs.push(rootDir + '/node_modules/' + (o.name || o.module) + '/')
-    }
-    taskDirs.push(rootDir + '/node_modules/gulp-sugar-simple/lib/')
-    taskDirs.push(rootDir + '/node_modules/gulp-sugar-tasks/lib/')
-    taskDirs.push(rootDir + '/node_modules/gulp-sugar/lib/')
-    taskDirs.push(rootDir + '/lib/')
-    taskDirs.push('')
+    if (typeof o === 'object' && o !== null) {
+      o = o || {}
+      var projectDir = module.parent.filename.replace(/^(.+)\/[^\/\\]+$/, '$1')
+      var rootDir = findNodeModulesDir(projectDir)
+      var taskDirs = []
 
-    var added = 0
-    var taskNames = []
-    for (var taskName in o) {
-      if (taskName === 'dest' || taskName === 'src' || taskName === 'module') {
-        continue
-      }
-      var taskConf = extend({}, o[taskName])
-
-      // Allow placing .src and .dest on the parent object
-      if (!taskConf.src && o.src) taskConf.src = o.src
-      if (!taskConf.dest && o.dest) taskConf.dest = o.dest
-
-      var deps = taskConf.deps || []
-      if (typeof deps === 'string') {
-        deps = deps.split(/\s+/)
-      }
-      var taskScript = taskConf.task || taskName
-      var task = null
-      var taskPath = ''
-
-      if (taskConf.skip) {
-        task = function () {
-          var skip = taskName
-          return function () {
-            console.log('Skipping task', skip)
-          }
+      // Check a handful of dirs for task definitions
+      taskDirs.push(rootDir + '/' + (o.taskDir || 'gulp') + '/')
+      if (process.env.GULP_SUGAR_TASKS) {
+        var envTasks = process.env.GULP_SUGAR_TASKS
+        if (envTasks[0] !== '/') {
+          envTasks = rootDir + '/' + envTasks
         }
-      } else {
-        for (var i = 0; i < taskDirs.length; i += 1) {
-          taskPath = taskDirs[i] + taskScript + '.js'
-          if (fs.existsSync(taskPath)) {
-            try {
-              task = require(taskPath)
-            } catch (e) {
-              console.warn('Error loading "' + taskPath + '"\n\t' + e.message)
-              return 1
+        if (envTasks[envTasks.length - 1] !== '/') {
+          envTasks = envTasks + '/'
+        }
+        taskDirs.push(envTasks)
+      }
+
+      if (o.name || o.module) {
+        taskDirs.push(rootDir + '/node_modules/' + (o.name || o.module) + '/')
+      }
+      taskDirs.push(rootDir + '/node_modules/gulp-sugar-simple/lib/')
+      taskDirs.push(rootDir + '/node_modules/gulp-sugar-tasks/lib/')
+      taskDirs.push(rootDir + '/node_modules/gulp-sugar/lib/')
+      taskDirs.push(rootDir + '/lib/')
+      taskDirs.push('')
+
+      var added = 0
+      var taskNames = []
+      for (var taskName in o) {
+        if (taskName === 'dest' || taskName === 'src' || taskName === 'module') {
+          continue
+        }
+        var oldSuper = this.tasksSeen[taskName]
+        if (oldSuper) {
+          die(1,
+          'Error: Task ' + taskName + ' already defined in superTask ' +
+                                        oldSuper + '\nplease rename it')
+        }
+        this.tasksSeen[taskName] = superTask
+
+        var taskConf = extend({}, o[taskName])
+
+        // Allow placing .src and .dest on the parent object
+        if (!taskConf.src && o.src) taskConf.src = o.src
+        if (!taskConf.dest && o.dest) taskConf.dest = o.dest
+
+        var deps = taskConf.deps || []
+        if (typeof deps === 'string') {
+          deps = deps.split(/\s+/)
+        }
+        deps = tasksOrDeps.concat(deps)
+        var taskScript = taskConf.task || taskName
+        var task = null
+        var taskPath = ''
+
+        if (taskConf.skip) {
+          task = function () {
+            var skip = taskName
+            return function () {
+              console.log('Skipping task', skip)
             }
           }
-          if (task) break
+        } else {
+          for (var i = 0; i < taskDirs.length; i += 1) {
+            taskPath = taskDirs[i] + taskScript + '.js'
+            if (fs.existsSync(taskPath)) {
+              try {
+                task = require(taskPath)
+              } catch (e) {
+                die(3, 'Error loading "' + taskPath + '"\n\t' + e.message + '\n' +
+                        e.stack)
+              }
+            }
+            if (task) break
+          }
         }
-      }
 
-      if (typeof task !== 'function') {
-        throw new Error("Couldn't find task " + taskScript + ' in \n\t' +
-          taskDirs.join('\n\t'))
+        if (typeof task !== 'function') {
+          die(2,
+            "Couldn't find task " + taskScript + ' in \n\t' + taskDirs.join('\n\t'))
+        }
+
+        this.gulp.task(taskName, deps.concat(taskNames), task(this.gulp, taskConf))
+        taskNames.push(taskName)
+        added += 1
       }
-      this.gulp.task(taskName, deps.concat(taskNames), task(this.gulp, taskConf))
-      taskNames.unshift(taskName)
-      added += 1 // TODO: Handle added tasks
+      this.gulp.task(superTask, taskNames)
+      this.gulp.task('default', [superTask])
     }
-    this.gulp.task(superTask, taskNames.concat(maybeDeps))
-    this.gulp.task('default', [superTask])
     return this
   }
-  skip (superTask, maybeDeps, o) {
+  skip (superTask, tasksOrDeps, o) {
     console.log('Skipping', superTask)
   }
+}
+
+function die (code, msg) {
+  console.warn(msg)
+  process.exit(code)
 }
 
 function findNodeModulesDir (fromDir) {
